9d63a2c10f9522199fada57ad5298fcd5eac1d8f
RealAbishan <highoncode09@gmail.com>
2023-10-04 11:53:43 +0530

    Initial Project Setup

---
+++ src/main/java/com/thefilmsiteproduceservice/TheFilmSiteProduceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thefilmsiteproduceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheFilmSiteProduceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheFilmSiteProduceServiceApplication.class, args);
+	}
+
+}
